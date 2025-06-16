2980f77de616d9037dca99885983dce60950fffb
OliverW <a163235304@163.com>
2025-06-16 22:51:57 +0800

    Add files via upload

---
+++ top10umi3.sh
@@ -0,0 +1,11 @@
+export TMPDIR="/data5/Wuky/tmp"
+
+# 创建临时目录（如果不存在）
+mkdir -p "$TMPDIR"
+
+# 执行原命令，sort 使用 -T 参数指定临时目录
+zcat XM-QGY-MME-2_L5_1.fq.gz | \
+awk 'NR%4==2 {count[$0]++} END {for (seq in count) print count[seq], seq}' | \
+sort -nr -T "$TMPDIR" | \
+head -10
+
