191c3dfcb90d242f85448527c43e68ffe50cd0eb
OliverW <a163235304@163.com>
2025-06-16 22:55:51 +0800

    Update and rename top10umi3.sh to stat/top10umi3.sh

--- stat/top10umi3.sh
+++ stat/top10umi3.sh
@@ -1,11 +1,12 @@
+####主要是用于统计占前10份额的reads是些什么？
+##22:55 2025/6/16 wky
 export TMPDIR="/data5/Wuky/tmp"
 
-# ������ʱĿ¼����������ڣ�
+# 创建临时目录（如果不存在）
 mkdir -p "$TMPDIR"
 
-# ִ��ԭ���sort ʹ�� -T ����ָ����ʱĿ¼
+# 执行原命令，sort 使用 -T 参数指定临时目录
 zcat XM-QGY-MME-2_L5_1.fq.gz | \
 awk 'NR%4==2 {count[$0]++} END {for (seq in count) print count[seq], seq}' | \
 sort -nr -T "$TMPDIR" | \
 head -10
-
